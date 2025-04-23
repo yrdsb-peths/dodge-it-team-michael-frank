aedd9012de5b829f78470e922b364e8255b3642a
xxMichaelxx1 <woushinidie@gmail.com>
2025-04-23 09:07:13 -0400

    add the game over and skeleton

--- MyWorld.java
+++ MyWorld.java
@@ -1,9 +1,17 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class MyWorld extends World {
-    public MyWorld() {
+public class MyWorld extends World
+{
+
+    
+    public MyWorld()
+    {    
         super(600, 400, 1);
-        Hero hero = new Hero();
         
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
+        
+        Spider spider = new Spider();
+        addObject(spider, 600, 100);
     }
 }
---
+++ Spider.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Spider extends Actor
+{
+    
+    public void act()
+    {
+         move(-10);
+        
+        if(getX() <= 0) {
+            resetSpider();
+        }
+        
+        if (isTouching(Hero.class)){
+            // Add a gameover symbol
+            Skeleton skeleton = new Skeleton();
+            getWorld().addObject(skeleton, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    public void resetSpider(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
+    }
+}
